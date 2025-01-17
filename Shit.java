fdf577d198964d7b38b8618864bd3618aa52ac56
Meteor - Victim Z <108459033+Victim-Z@users.noreply.github.com>
2025-01-17 09:52:59 +0800

    Add files via upload

---
+++ Shit.java
@@ -0,0 +1,8 @@
+public class Shit{
+   public Shit(String name){
+      System.out.println(name ); 
+   }
+   public static void main(String[] args){
+      Shit fuckinShit = new Shit("Fuck");
+   }
+}
